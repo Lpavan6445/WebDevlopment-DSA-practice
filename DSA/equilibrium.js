ee9d6b224704e3db417a46d19704f69b6d1cbd5b
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2021-11-10 15:58:44 +0530

    equilibriulm.js
    find index of element where left and right element sums are equal.  else -1

--- DSA/equilibrium.js
+++ DSA/equilibrium.js
@@ -1,1 +1,21 @@
-<!--Find Equilibrium Element -->
+// <!--Find Equilibrium Element -->
+function checkEquil(n,arr){
+  var sum=0;
+    for(var i=0; i<n; i++){
+        sum+=arr[i]
+    }
+  var right=0;
+    for(var i=0; i<n; i++){
+        sum-=arr[i]
+        if(right == sum){
+            return i+1
+        }else{
+            right+=arr[i]
+        }
+    }
+    return -1;
+}
+
+var arr = [3,3,5,5,1]
+var n = arr.length;
+console.log(checkEquil(n,arr))
