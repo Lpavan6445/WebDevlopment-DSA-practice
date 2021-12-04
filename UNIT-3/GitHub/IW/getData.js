cd5d5369a0eaa10d3527fceb289019c462d96a79
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2021-12-04 21:31:11 +0530

    getData

---
+++ UNIT-3/GitHub/IW/getData.js
@@ -0,0 +1,12 @@
+async function getData(user){
+  try{
+   var response = await fetch(`https://api.github.com/users/${user}`)
+    var data = await response.json()
+
+    return data
+  }catch(errors){
+      console.log("Err:", err)
+  }
+
+}
+export {getData}
