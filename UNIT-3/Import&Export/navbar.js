19f6b25405892523263568e4c6bdeb6f30ab62c7
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2021-12-05 22:56:07 +0530

    navbar

---
+++ UNIT-3/Import&Export/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+    return `<div id="navbar">
+    <h3><a href="./IW.html">Home</a></h3>
+     <div id="options">
+        <h3><a href="./electronics.html">Electronics</a></h3>
+        <h3><a href="./Jewellery.html">Jewellery</a></h3>
+        <h3><a href="http://">Sign In</a></h3>
+        <h3><a href="http://">Sign Up</a></h3>
+     </div>
+     </div>`
+}
+
+export default navbar
