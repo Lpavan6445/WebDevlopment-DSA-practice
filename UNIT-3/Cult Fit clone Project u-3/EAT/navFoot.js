b524d7e4dcbde6d5d41d6ea9ef44bd506249a0ac
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2021-12-18 22:44:48 +0530

    navFoot

--- UNIT-3/Cult Fit clone Project u-3/EAT/navFoot.js
+++ UNIT-3/Cult Fit clone Project u-3/EAT/navFoot.js
@@ -1,140 +1,177 @@
 function navbar(){
-    return `<div class="headerDropDown" id="headDropDown">
-    <div class="dropDownElem">
-         <div class="image-wrapper " style="">
-           <img alt="" src="https://static.cure.fit/assets/images/cult-icon.svg">
-         </div>   
-        <div class="css-1daiy-toolTipContent">
-          CULT
-        </div>
-    </div>
+  return `<div class="headerDropDown" id="headDropDown">
   <div class="dropDownElem">
-         <div class="image-wrapper " style="">
-           <img alt="" src="https://static.cure.fit/assets/images/Livefit.svg">
-         </div>   
-        <div class="css-1daiy-toolTipContent">
-          LIVE
-        </div>
-    </div>
-  <div onclick="window.location.href='./EAT/eatordernow.html'" class="dropDownElem">
-         <div class="image-wrapper " style="">
-           <img alt="" src="https://static.cure.fit/assets/images/eat-hover-icon.svg">
-         </div>   
-        <div class="css-1daiy-toolTipContent">
-          EAT
-        </div>
-    </div>
-  <div class="dropDownElem">
-         <div class="image-wrapper " style="">
-           <img  alt="" src="https://static.cure.fit/assets/images/mind-icon.svg">
-         </div>   
-        <div class="css-1daiy-toolTipContent">
-          MIND
-        </div>
-    </div>
-  <div class="dropDownElem">
-         <div class="image-wrapper " style="">
-           <img alt="" src="https://static.cure.fit/assets/images/carefit-blk.svg">
-         </div>   
-        <div class="css-1daiy-toolTipContent">
-          CARE
-        </div>
-    </div>
-  <div class="dropDownElem">
-         <div class="image-wrapper " style="">
-           <img alt="" src="https://static.cure.fit/assets/images/store-icon.svg">
-         </div>   
-        <div class="css-1daiy-toolTipContent">
-          STORE
-        </div>
-    </div>
- </div>
- <div id="innerNavbar">
-    <div id="logo" class="logo">
-        <a href="home.html">
-            <img src="https://static.cure.fit/assets/images/curefit-v-man.svg" alt="cult fit logo">
-        </a>
-        <div id="spet">
+       <div class="image-wrapper " style="">
+         <img alt="" src="https://static.cure.fit/assets/images/cult-icon.svg">
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        CULT
+      </div>
+  </div>
+<div class="dropDownElem">
+       <div class="image-wrapper " style="">
+         <img alt="" src="https://static.cure.fit/assets/images/Livefit.svg">
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        LIVE
+      </div>
+  </div>
+<div onclick="window.location.href='C:/Users/pavan/Desktop/MASAI SCHOOL/Projects/Cult Fit clone Project u-3/EAT/eatordernow.html'" class="dropDownElem">
+       <div class="image-wrapper " style="">
+         <img alt="" src="https://static.cure.fit/assets/images/eat-icon.svg">
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        EAT
+      </div>
+  </div>
+<div class="dropDownElem">
+       <div class="image-wrapper " style="">
+         <img  alt="" src="https://static.cure.fit/assets/images/mind-icon.svg">
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        MIND
+      </div>
+  </div>
+<div class="dropDownElem">
+       <div class="image-wrapper " style="">
+         <img alt="" src="https://static.cure.fit/assets/images/carefit-blk.svg">
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        CARE
+      </div>
+  </div>
+<div class="dropDownElem">
+       <div class="image-wrapper " style="">
+         <img alt="" src="	https://static.cure.fit/assets/images/store-icon-hover.svg">
+       </div>   
+      <div class="css-1daiy-toolTipContent">
+        STORE
+      </div>
+  </div>
+</div>
+<div id="innerNavbar">
+  <div id="logo" class="logo">
+      <a href="home.html">
+          <img src="https://static.cure.fit/assets/images/curefit-v-man.svg" alt="cult fit logo">
+      </a>
+      <div id="spet">
+        <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/image/header-web/store_fit.svg" alt="store" class=" css-130q1oe-ImageElement efaomd31" loading="lazy" width="92.2" height="20">    
+        <img  id="dropdown-arrow" class="dropdownarrow" src="https://static.cure.fit/assets/images/arrow-clp.svg" alt="Expand menu" style="transform: rotate(180deg); min-height: 15px; min-width: 15px;">
+      </div>
+  </div>
+   <div id="eatSubpages">
+       <div>
+         <ul>
+           <li id="order"><a href="menproduct.html" >cultsport</a></li>
+         </ul>
+       </div>
+       
+   </div>
+  <div id="user-details" class="user-details">
+      <div id="location" class="location">
 
-             <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/image/header-web/eat-fit.svg" alt="eat" class=" css-130q1oe-ImageElement efaomd31" loading="lazy" width="66.66" height="19.999999999999996">
-           <img  id="dropdown-arrow" class="dropdownarrow" src="https://static.cure.fit/assets/images/arrow-clp.svg" alt="Expand menu" style="transform: rotate(180deg); min-height: 15px; min-width: 15px;">
-        </div>
-    </div>
-     <div id="eatSubpages">
-         <div>
-           <ul>
-             <li id="order"><a href="eatordernow.html" >Order</a></li>
-             <li><a href="">Meal Plans</a></li>
-           </ul>
-         </div>
-         
-     </div>
-    <div id="user-details" class="user-details">
-        <div id="location" class="location">
-
-            <div id="blue-dot" class="blue-dot"></div>
-            <div id="cityName" class="cityName">Bangalore</div>
-        </div>
-        <div id="logged-out" class="logged-out">
-            <img src="https://static.cure.fit/assets/images/user-image.svg" alt="login image">
-            <div id="login-text" class="login-text">Login</div>
-        </div>
-        <div id="cart" class="cart">
-            <img src="https://static.cure.fit/assets/images/cart-image.svg" alt="cart logo">
-        </div>
-    </div>
+          <div id="blue-dot" class="blue-dot"></div>
+          <div id="cityName" class="cityName">Bangalore</div>
+      </div>
+      <div id="profile">
+      <img src="https://profile-pictures-prod.s3.amazonaws.com/5491b50b-b284-490f-a55d-db97431b1e32" style="max-width:100%;max-height:90%">
+      </div>
+      <div id="logged-out" class="logged-out">
+      <div>
+        <img id="image11" src="https://cdn-icons-png.flaticon.com/512/25/25623.png" />
+      </div>
+     <div id="logindata">
+    
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/profile-icon.svg" />
+              <a class="allat" >Profile</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/cart-black.svg" />
+              <a class="allat" >Orders</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/health-profile-black.svg" />
+              <a class="allat" >Medical Records</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/combined-shape-black.svg" />
+              <a class="allat" >Active Packs</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/coupon-black.svg" />
+              <a class="allat" >Redeem Vouchers</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/settings-black.svg" />
+              <a class="allat" >Account</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/support-black.svg" />
+              <a class="allat" >Support</a>
+          </div>
+          <div id="list">
+              <img class="imgall" src="https://static.cure.fit/assets/images/logout-black.svg" />
+              <a class="allat" href="">Logout</a>
+          </div>
+  </div>
+      </div>
+      <div id="cart" class="cart">
+          <img id="cartNik" src="https://static.cure.fit/assets/images/cart-image.svg" alt="cart logo">
+      </div>
+  </div>
 </div>`
 }
 function footer(){
-    return ` <div id="Nikfooter">
-    <div id="footerd1">
-        <img id="footerd1I" src="https://static.cure.fit/assets/images/cult-logo-white.svg" alt="">
-        <p id="footerd1P">At cult.fit, we make group workouts fun, daily food healthy & tasty, mental fitness easy with yoga & meditation, and
-     medical & lifestyle care hassle-free. #BeBetterEveryDay</p>
-     </div>
-    <div id="footerd2">
-        <p style="margin-top: 10%;" class="nikContact">CONTACT US</p>
-        <p class="nikContact">BLOG</p>
-        <p class="nikContact">PARTNER.FIT</p>
-        <p class="nikContact">TALKS</p>
-        <P class="nikContact">CAREERS</P>
-    </div>
-    <div id="footerd3">
-         <p class="nikContact1">CULTFIT FOR BUSINESSES</p>
-         <p class="nikContact1">CULT FRANCHISE</p>
-         <p class="nikContact1">CULT PASS NETWORK</p>
-         <P class="nikContact1">CORPORATE PARTNERSHIPS</P>
-    </div>
-    <div id="footerd4"></div>
+  return ` <div id="Nikfooter">
+  <div id="footerd1">
+      <img id="footerd1I" src="https://static.cure.fit/assets/images/cult-logo-white.svg" alt="">
+      <p id="footerd1P">At cult.fit, we make group workouts fun, daily food healthy & tasty, mental fitness easy with yoga & meditation, and
+   medical & lifestyle care hassle-free. #BeBetterEveryDay</p>
+   </div>
+  <div id="footerd2">
+      <p style="margin-top: 10%;" class="nikContact">CONTACT US</p>
+      <p class="nikContact">BLOG</p>
+      <p class="nikContact">PARTNER.FIT</p>
+      <p class="nikContact">TALKS</p>
+      <P class="nikContact">CAREERS</P>
+  </div>
+  <div id="footerd3">
+       <p class="nikContact1">CULTFIT FOR BUSINESSES</p>
+       <p class="nikContact1">CULT FRANCHISE</p>
+       <p class="nikContact1">CULT PASS NETWORK</p>
+       <P class="nikContact1">CORPORATE PARTNERSHIPS</P>
+  </div>
+  <div id="footerd4"></div>
 
-    <!-- app store part -->
-    <div id="footerd4">
-             <a href="https://apps.apple.com/us/app/cure-fit/id1217794588">
-                 <img style="margin-left: 25%;margin-top: 20%;" src="https://static.cure.fit/assets/images/app-store.svg" alt="">
-             </a>
-             <a href="https://play.google.com/store/apps/details?id=fit.cure.android">
-                 <img style="margin-left: 25%;margin-top: 10%;" src="https://static.cure.fit/assets/images/play-store.svg" alt="">
-             </a>
-    </div>
- </div>
- <div style="display: flex;color: whitesmoke;margin-top: -50px;" id="smallContact">
-     <p style="margin-left: 5%;">CONTACT US</p>
-     <p  style="margin-left: 5%;">FAQ</p>
-     <p  style="margin-left: 5%;">Terms & conditions</p>
-     <p  style="margin-left: 5%;">Privacy Policy</p>
-     <p  style="margin-left: 5%;">Refund Policy</p>
-     <a href="https://www.facebook.com/cultfitofficial">
-        <img style="margin-left: 300px;margin-top: 20px;"src="https://static.cure.fit/assets/images/facebook_icon_white.svg" alt="">
-     </a>
-     <a href="https://twitter.com/cultfitOfficial">
-         <img style="margin-left: 50px;margin-top: 20px;" src="https://static.cure.fit/assets/images/twitter_icon_white.svg" alt="">
-     </a>
-     <a href="https://www.instagram.com/cultfitOfficial/">
-             <img style="margin-left: 50px;margin-top: 20px;" src="https://static.cure.fit/assets/images/instagram_icon_white.svg" alt="">
-     </a>
-     <a href="https://www.youtube.com/channel/UCSE72IaHOL-1Tv-m3JHE4Cg">
-         <img style="margin-left: 50px;margin-top: 20px;" src="https://static.cure.fit/assets/images/youtube_icon_white_1.svg" alt="">
-     </a>
- </div>`
+  <!-- app store part -->
+  <div id="footerd4">
+           <a href="https://apps.apple.com/us/app/cure-fit/id1217794588">
+               <img style="margin-left: 25%;margin-top: 20%;" src="https://static.cure.fit/assets/images/app-store.svg" alt="">
+           </a>
+           <a href="https://play.google.com/store/apps/details?id=fit.cure.android">
+               <img style="margin-left: 25%;margin-top: 10%;" src="https://static.cure.fit/assets/images/play-store.svg" alt="">
+           </a>
+  </div>
+</div>
+<div style="display: flex;color: whitesmoke;margin-top: -50px;" id="smallContact">
+   <p style="margin-left: 5%;">CONTACT US</p>
+   <p  style="margin-left: 5%;">FAQ</p>
+   <p  style="margin-left: 5%;">Terms & conditions</p>
+   <p  style="margin-left: 5%;">Privacy Policy</p>
+   <p  style="margin-left: 5%;">Refund Policy</p>
+   <a href="https://www.facebook.com/cultfitofficial">
+      <img style="margin-left: 300px;margin-top: 20px;"src="https://static.cure.fit/assets/images/facebook_icon_white.svg" alt="">
+   </a>
+   <a href="https://twitter.com/cultfitOfficial">
+       <img style="margin-left: 50px;margin-top: 20px;" src="https://static.cure.fit/assets/images/twitter_icon_white.svg" alt="">
+   </a>
+   <a href="https://www.instagram.com/cultfitOfficial/">
+           <img style="margin-left: 50px;margin-top: 20px;" src="https://static.cure.fit/assets/images/instagram_icon_white.svg" alt="">
+   </a>
+   <a href="https://www.youtube.com/channel/UCSE72IaHOL-1Tv-m3JHE4Cg">
+       <img style="margin-left: 50px;margin-top: 20px;" src="https://static.cure.fit/assets/images/youtube_icon_white_1.svg" alt="">
+   </a>
+</div>`
 }
 export {navbar,footer}
