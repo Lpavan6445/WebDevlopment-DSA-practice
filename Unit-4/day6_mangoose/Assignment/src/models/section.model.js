fcf81edf59b8f7f24515ac3d3fc2966f21b43d32
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2022-01-03 22:43:49 +0530

    done

--- Unit-4/day6_mangoose/Assignment/src/models/section.model.js
+++ Unit-4/day6_mangoose/Assignment/src/models/section.model.js
@@ -2,7 +2,17 @@ const mongoose = require("mongoose")
 
 const sectionSchema = new mongoose.Schema(
     {
-        section_name:{type:String, require:true}
+        section_name:{type:String, require:true},
+        books_id:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"book",
+            required:true,
+        },
+        author_id:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"author",
+            required:true,
+        }
     },
     { 
         versionKey: false,
