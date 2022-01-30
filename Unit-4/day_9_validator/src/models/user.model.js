92cf1cfdd0568e7c012d58cd614eb69c7a7e9804
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2022-01-30 23:18:05 +0530

    Update user.model.js

--- Unit-4/day_9_validator/src/models/user.model.js
+++ Unit-4/day_9_validator/src/models/user.model.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     first_name:{type:String, required:true},
     last_name:{type:String, required:true},
     email:{type:String, required:true},
-    pincode:{type:String, required:true},
-    age:{type:String, required:true},
+    pincode:{type:Number, required:true},
+    age:{type:Number, required:true},
     gender:{type:String, required:true}
 })
 
