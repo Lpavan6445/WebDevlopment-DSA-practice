30636327956d26d8317ab3eba0e8ed0322d18e2f
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2022-02-01 23:38:07 +0530

    Create App.js

---
+++ Unit-5_React/d4_/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import "./components/grocery.css";
+import {Grocery} from "./components/Grocery"
+export default function App() {
+  return (
+    <div className="App">
+        <Grocery/>
+    </div>
+  );
+}
