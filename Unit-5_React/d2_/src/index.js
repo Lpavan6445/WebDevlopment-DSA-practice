495600a956313692021eab9b3856b6f3d38318a6
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2022-02-01 23:39:18 +0530

    Create index.js

---
+++ Unit-5_React/d2_/src/index.js
@@ -0,0 +1,16 @@
+// import css from "./style.css"
+// import img from "./images.png"
+
+// var logo = document.getElementById("logo")
+// logo.setAttribute("src",img)
+// console.log(img)
+
+// var noteInput = document.getElementById("noteInput")
+// let submit = document.getElementById("submit")
+
+// submit.onclick=function(){
+//     let show = document.getElementById("show")
+//     let create = document.createElement("h3")
+//     create.innerHTML=noteInput.value
+//     show.appendChild(create)
+// }
