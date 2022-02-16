8a28232681dc494692edf6e32a4d1b39fa025b3c
Lpavan6445 <87610669+Lpavan6445@users.noreply.github.com>
2022-02-16 22:38:41 +0530

    Update main.jsx

--- Unit-5_React/route2/vite-project/src/main.jsx
+++ Unit-5_React/route2/vite-project/src/main.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-
+import { Provider} from 'react-redux'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { store } from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   
+      <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        </BrowserRouter>
+      </Provider>
+    
+     
   </React.StrictMode>,
   document.getElementById('root')
 )
